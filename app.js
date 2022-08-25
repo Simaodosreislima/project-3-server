62fa4d6c1d29b6d67649ca4a57cb4988b47ac72e
Simão Lima <simaodosreislima@gmail.com>
2022-08-25 22:43:25 +0100

    solved authRoutes problem. Missed TOKEN_SECRET

--- app.js
+++ app.js
@@ -13,14 +13,12 @@ const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
-
+const { isAuthenticated } = require("./middleware/jwt.middleware");
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes/index.routes");
 app.use("/api", allRoutes);
 
-const authRouter = require("./routes/auth.routes");
-app.use("/auth", authRouter);
 
 /* const userRouter = require("./routes/user.routes");
 app.use("/api", userRouter) */
